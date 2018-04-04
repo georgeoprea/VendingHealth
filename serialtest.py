9a46a9ce46098931ddbb896e3b73639465c424be
georgeoprea <george.adrian.oprea@sap.com>
2018-04-04 17:29:02 +0300

    Get user details + refactor

--- serialtest.py
+++ serialtest.py
@@ -6,22 +6,53 @@ firebase = firebase.FirebaseApplication(FIREBASE_ROOT, None)
 
 serial_port = '/dev/ttyUSB0'
 baud_rate = 9600
+ser = serial.Serial(serial_port, baud_rate)
 
-def checkIDmatch( id ):
+def getUserByID( id ):
 	result = firebase.get('/Users', None)
-	# if 
+ 	user = result.get(id)
+	if(user == None):
+		print("\nNULL USER\n")
+		return None
+	else:
+		print("User::::")
+		print(user)
+		return user
 
 def extractCardID( line ):
 	# TODO: parse line to get card ID:
 	id = ""
 	# id = line.parse...
-	return id
+	# return id
+	return "47 C1 8D AB"  # TODO: for test
 
-ser = serial.Serial(serial_port, baud_rate)
+def extractProductCost( line ):
+	#parse line
+	#extract cost
+
+	return 200		# TODO: for test
+
+def getBalance( user ):
+	return user.get("balance")
+
 while True:
 	line = ser.readline()
 	line = line.decode("utf-8")
-	if "47 C1 8D AB" in line:
+	id = extractCardID( line )
+	cost = extractProductCost( line )
+
+	user = getUserByID(id)
+	if user == None:
+		print("Non Existent User")
+		continue
+
+	balance = getBalance(user)
+
+	print("balance:" + balance)
+
+	if (balance >= cost):
+		print("Vending...")
+
 		ser.write("y")
 		while True:
 			response = ser.readline()
@@ -31,3 +62,13 @@ while True:
 		else:
 			ser.write("n")
 		print(line)
+	# if id in line:
+	# 	ser.write("y")
+	# 	while True:
+	# 		response = ser.readline()
+	# 		print(line)
+	# 		if "done" in response:
+	# 			break
+	# 	else:
+	# 		ser.write("n")
+	# 	print(line)
